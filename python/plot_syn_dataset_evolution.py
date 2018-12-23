9dd4950f8fec8e97f1bd8afc55dee2a97ae3e59a
Ankur Handa <ahanda@nvidia.com>
2018-12-23 13:50:13 -0800

    adding more datasets
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- python/plot_syn_dataset_evolution.py
+++ python/plot_syn_dataset_evolution.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
+from collections import OrderedDict
 
 dataset_dict = {'NIST': 30,
+                'IKEA': 219,
                 'PrincetonSB': 6670,
                 'Thingi10K': 10000,
+                'ObjectNet3D': 44161,
                 'ModelNet': 151128,
                 # 'ShapeNetSem': 12000,
                 # 'ShapeNetCore': 51300,
@@ -11,17 +15,24 @@ dataset_dict = {'NIST': 30,
                 'ShapeNet': 3000000,
                 }
 
+dataset_dict = OrderedDict(sorted(dataset_dict.items(), key=lambda x: x[1]))
+
+# print(dataset_dict)
 
 dataset_name, datasets_size = list(dataset_dict.keys()), list(dataset_dict.values())
 fig, ax = plt.subplots()
 
 ind = np.arange(len(dataset_name))
 
-ax.bar(ind, np.log10(datasets_size), width=0.35,
-       color='aquamarine', label='datasets')
+ax.bar(ind[:-1], np.log10(datasets_size[:-1]), width=0.35,
+       color='aquamarine', #label='datasets'
+       )
 
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
+ax.bar(ind[-1], np.log10(datasets_size[-1]), width=0.35,
+       color='aquamarine', alpha=0.4)
+
+ax.set_ylabel('Dataset size (in logarithmic scale)')
+ax.set_title('Object datasets')
 ax.set_xticks(ind)
 ax.set_xticklabels(dataset_name)
 ax.legend()
